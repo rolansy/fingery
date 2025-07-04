954ace1c3da6fcd22d111a81c526bd4a6054c3be
rolansy <ronalshoeygeorge@gmail.com>
2025-07-05 02:50:36 +0530

    Refactor Firebase configuration in firebase.js to use environment variables for sensitive data, enhancing security and flexibility.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAPLbAAx6L2O5mnKW1DwebQ4kMNP4H5Cow",
-    authDomain: "fingery-39905.firebaseapp.com",
-    projectId: "fingery-39905",
-    storageBucket: "fingery-39905.firebasestorage.app",
-    messagingSenderId: "565151032424",
-    appId: "1:565151032424:web:2e7b6d9cf1be9e54edc7dd",
-    measurementId: "G-0MYK8W1KZ0"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
   };
 
 const app = initializeApp(firebaseConfig);
